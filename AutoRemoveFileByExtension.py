1ac89c06c04640077e868bd5f4fd07471a884254
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-12-08 04:25:28 +0900

    리소스 분리

--- AutoRemoveFileByExtension.py
+++ AutoRemoveFileByExtension.py
@@ -1,21 +1,37 @@
 from os import remove, walk
 from os.path import splitext
 from time import sleep
+import configparser
 
 
 class AutoRemoveFileByExtension:
-    def __init__(self, dir_list, ignore_dir_list):
-        self.dir_list = dir_list
-        self.ignore_dir_list = ignore_dir_list
+    def __init__(self, search_dir_list=None, ignore_dir_list=None):
+        if search_dir_list is None:
+            self.search_dir_list = self.readSetting("search_dir_list")
+        else:
+            self.search_dir_list = search_dir_list
+        if ignore_dir_list is None:
+            self.ignore_dir_list = self.readSetting("ignore_dir_list")
+        else:
+            self.ignore_dir_list = ignore_dir_list
 
-    def search(self, remove_file_extension, remove_file=False):
-        if remove_file_extension == "*" and remove_file == True:
+    def readSetting(self, dir):
+        result = []
+        with open(dir, "r") as r:
+            temp = r.readlines()
+            for t in temp:
+                result.append(t.strip())
+        return result
+
+    def search(self, remove_file_extension, remove_file="False"):
+        remove_file = remove_file.lower()
+        if remove_file_extension == "*" and remove_file == "true":
             for _ in range(100):
                 print("주의!! 모든 파일이 삭제됩니다!")
                 sleep(0.1)
             if input("작업을 수행하려면 remove를 입력하세요 : ") != "remove":
                 return
-        for dir in self.dir_list:
+        for dir in self.search_dir_list:
             for (path, _, files) in walk(dir):
                 breaker = False
                 for ignore in self.ignore_dir_list:
@@ -30,27 +46,20 @@ class AutoRemoveFileByExtension:
                     file_pull_path = f"{path}\\{filename}"
                     if remove_file_extension == "*":
                         print(file_pull_path)
-                        if remove_file:
+                        if remove_file == "true":
                             remove(file_pull_path)
                     elif file_extension == remove_file_extension:
                         print(file_pull_path)
-                        if remove_file:
+                        if remove_file == "true":
                             remove(file_pull_path)
 
 
 if __name__ == '__main__':
-    dir_list = [
-        "c:/",
-        "d:/"
-        # os.path.expanduser('~')
-        # os.path.join(os.path.expanduser('~'), 'Desktop'),
-        # os.path.join(os.path.expanduser('~'), 'Documents')
-    ]
+    obj = AutoRemoveFileByExtension()
+    config = configparser.ConfigParser()
+    config.read("setting.ini")
 
-    ignore_dir_list = [
-        "Program Files",
-        "AppData",
-        "Lib"
-    ]
-    obj = AutoRemoveFileByExtension(dir_list, ignore_dir_list)
-    obj.search("*", remove_file=False)
+    obj.search(
+        remove_file_extension=config["search"]["remove_file_extension"],
+        remove_file=config["search"]["remove_file"]
+    )
